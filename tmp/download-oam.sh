c06a41d91554cb0b245f088c510e6f37193a60d4
lgudyma <lgudyma@kontur.io>
2022-08-23 16:06:01 +0300

    changing docker to ubuntu because of bad wget

--- tmp/download-oam.sh
+++
@@ -1,9 +0,0 @@
-# Directory for storing oam-images, symlink to dir
-ln -sf /mnt/evo4tb/oam_images data/in/oam_images/
-
-curl https://api.openaerialmap.org/meta | jq '.meta.found' | \
-    awk '{print int($0/100)+1}' | xargs -I {} seq {} | \
-    xargs -I {} curl https://api.openaerialmap.org/meta?page={} | jq -c '.results' > data/in/oam_images/oam_meta.json
-
-cat data/in/oam_images/oam_meta.json | jq -r '.[].uuid' | \
-    parallel --progress -j 16 wget -nc -q -P oam-images {}
