83faf4d704aabe873a0b80ad58a093e4b6866d01
isaricicek <isaricicek@kontur.io>
2022-08-31 15:22:37 +0300

    11927 added control for mounted folder

--- tiler/main.js
+++ tiler/main.js
@@ -2,23 +2,39 @@ const config = require("./utils/config-helper").getConfig();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const fs = require("fs");
 const landing = require("./routes/landing");
 const footPrint = require("./routes/footPrint");
 const oamTiles = require("./routes/oamTiles");
 const reorTiles = require("./routes/reorTiles");
 const health = require('@cloudnative/health-connect');
 const live = require('./services/live');
+var path = require('path');
 
 const app = express();
 
 let healthCheck = new health.HealthChecker();
+let fileExists = false;
 
-//is db up
-const livePromise = () => new Promise(async (resolve, _reject) => {
+//is db up & mount folder is not empty
+const livePromise = () => new Promise(async (resolve, reject) => {
+  var oam_path = path.resolve(__dirname, config.app.oam_path);
+  fileExists = await fs.promises.readdir(oam_path)
+ 
+    // If promise resolved and
+    // data are fetched
+    .then(files => {
+      return files.length === 0;
+    })
+ 
+    // If promise is rejected
+    .catch(err => {
+        console.log(err)
+    })
 
   const appFunctioning = await live.isLive();
 
-  if (appFunctioning[0]) {
+  if (appFunctioning[0] && !fileExists) {
     resolve();
   } else {
     reject(new Error("App is not functioning correctly"));
