1e074d17243d1dee2b1d0766f2d0339e75985598
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-09-09 11:11:12 +0300

    fix

--- mosaic-tiler/push_docker_images.sh
+++ mosaic-tiler/push_docker_images.sh
@@ -3,5 +3,5 @@
 set -e
 echo "PLEASE NOTE: you need to docker login with your own credentials into Kontur's nexus to push docker image"
 
-(cd tiler && docker build -t nexus.kontur.io:8085/konturdev/raster-tiler .)
+docker build -t nexus.kontur.io:8085/konturdev/raster-tiler .
 docker push nexus.kontur.io:8085/konturdev/raster-tiler
