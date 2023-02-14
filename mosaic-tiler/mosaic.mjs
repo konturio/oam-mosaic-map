07a7ac738ce3150edd9d4d8a1b69b0690e63503a
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-02-14 17:29:51 +0300

    Move getTileCover in separate file

--- mosaic-tiler/mosaic.mjs
+++ mosaic-tiler/mosaic.mjs
@@ -1,5 +1,4 @@
 import sharp from "sharp";
-import cover from "@mapbox/tile-cover";
 import * as db from "./db.mjs";
 import { cacheGet, cachePut, cacheDelete, cachePurgeMosaic } from "./cache.mjs";
 import { Tile, TileImage, constructParentTileFromChildren } from "./tile.mjs";
@@ -7,6 +6,7 @@ import {
   enqueueTileFetching,
   enqueueMetadataFetching,
 } from "./titiler_fetcher.mjs";
+import { getTileCover } from "./tile_cover.mjs";
 
 const TITILER_BASE_URL = process.env.TITILER_BASE_URL;
 const TILE_SIZE = 512;
@@ -82,46 +82,6 @@ async function getGeotiffMetadata(uuid) {
       .replace("___x___", "{x}")
       .replace("___y___", "{y}"),
   };
-}
-
-const tileCoverCache = {
-  0: new WeakMap(),
-  1: new WeakMap(),
-  2: new WeakMap(),
-  3: new WeakMap(),
-  4: new WeakMap(),
-  5: new WeakMap(),
-  6: new WeakMap(),
-  7: new WeakMap(),
-  8: new WeakMap(),
-  9: new WeakMap(),
-  10: new WeakMap(),
-  11: new WeakMap(),
-  12: new WeakMap(),
-  13: new WeakMap(),
-  14: new WeakMap(),
-  15: new WeakMap(),
-  16: new WeakMap(),
-  17: new WeakMap(),
-  18: new WeakMap(),
-  19: new WeakMap(),
-  20: new WeakMap(),
-  21: new WeakMap(),
-  22: new WeakMap(),
-  23: new WeakMap(),
-  24: new WeakMap(),
-  25: new WeakMap(),
-};
-
-function getTileCover(geojson, zoom) {
-  if (tileCoverCache[zoom].get(geojson)) {
-    return tileCoverCache[zoom].get(geojson);
-  }
-
-  const tileCover = cover.tiles(geojson, { min_zoom: zoom, max_zoom: zoom });
-  tileCoverCache[zoom].set(geojson, tileCover);
-
-  return tileCover;
 }
 
 // request tile for single image
@@ -405,7 +365,7 @@ async function invalidateMosaicCache() {
     // to rebuild "mosaic" cache.
     await cachePurgeMosaic();
   } else {
-    await Promise.all(invalidTilePaths.map(path => cacheDelete(path)));
+    await Promise.all(invalidTilePaths.map((path) => cacheDelete(path)));
   }
 
   await cachePut(
