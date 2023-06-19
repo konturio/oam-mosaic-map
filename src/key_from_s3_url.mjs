ae26d8623b802939485e5d06ac987f74d8dac867
dqunbp <dqunbp@gmail.com>
2023-06-19 12:54:26 +0300

    Fix the cache key extraction

--- src/key_from_s3_url.mjs
+++ src/key_from_s3_url.mjs
@@ -1,5 +1,5 @@
 function keyFromS3Url(url) {
-  const regex = /^https?:\/\/[^\/]+\.s3[^\/]*\.amazonaws\.com.*\/(.+)\.tif$/;
+  const regex = /^https?:\/\/[^\/]+\.s3[^\/]*\.amazonaws\.com\/(.+)\.tif$/;
 
   const match = url.match(regex);
   return match ? match[1] : null;
